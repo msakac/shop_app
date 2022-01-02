7734b1d413cfb586b144ad0859a036e0288323e8
msakac <msakac@foi.hr>
2022-01-02 15:19:14 +0100

    Added GestureDetector to the Image. GestureDetector has OnTap() function that navigates to ProductDetailScreen with arguments. I used .push with MaterialPageRoute to navigate to new screen.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -12,18 +13,24 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GridTile(
-        child: Image.network(imageUrl, fit: BoxFit.cover),
+        child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title)),
+              );
+            },
+            child: Image.network(imageUrl, fit: BoxFit.cover)),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
             icon: Icon(Icons.favorite),
             onPressed: () {},
-            color:Theme.of(context).accentColor,
+            color: Theme.of(context).accentColor,
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             onPressed: () {},
-            color:Theme.of(context).accentColor,
+            color: Theme.of(context).accentColor,
           ),
           title: Text(
             title,
