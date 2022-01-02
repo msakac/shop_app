c6dec8f50de1115bae201b6f262b4345a13fc51d
msakac <msakac@foi.hr>
2022-01-02 14:49:59 +0100

    Adding ProductsOverviewScreen as home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -12,21 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShopp'),
-      ),
-      body: Center(
-        child:  Text('Time to have fun'),
-      ),
-    );
-  }
-}
