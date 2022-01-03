f662832be852cd50d090138b64b7176a203f46b9
msakac <msakac@foi.hr>
2022-01-03 09:54:14 +0100

    Added enum FilterOptions with Favorites and All. Created PopupMenuButton to switch between Favorite and All products. Depending on which button was choosen we set the enum value and with provider we call method showFavoritesOnly() or showAll()

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,17 +1,45 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
+
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+     final products = Provider.of<Products>(context, listen:false);
     return Scaffold(
-      appBar: AppBar(title: Text('MyShop')),
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              print(selectedValue);
+              if(selectedValue == FilterOptions.Favorites){
+                products.showFavoritesOnly();
+              }else{
+                products.showAll();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(child: Text('Only favorites'), value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All),
+            ],
+          ),
+        ],
+      ),
       body: ProductsGrid(),
     );
   }
 }
-
-
