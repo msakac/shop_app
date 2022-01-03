1f7fdcafe1229cbdbf5bba326b734440c82afa58
msakac <msakac@foi.hr>
2022-01-03 11:11:29 +0100

    Added onPressed() function for shopping cart IconButton. Function calls cart.addItem

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -13,7 +14,8 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-    print('rebuild'); //now we rebuild only IconButton wih Consumer<Product> widget
+    final cart = Provider.of<Cart>(context, listen: false);
+
       return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GridTile(
@@ -36,7 +38,9 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+            },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
