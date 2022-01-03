a6eabd4a403a5f38ccfc14a9bc77247ba584d191
msakac <msakac@foi.hr>
2022-01-03 14:08:31 +0100

    Added drawer to the Scaffold widget

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,10 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
 import '../screens/cart_screen.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions {
   Favorites,
@@ -62,6 +64,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
