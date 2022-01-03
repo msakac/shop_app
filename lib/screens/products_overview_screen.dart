56e477ea5213c6586acc1bcc1453e0b61887d685
msakac <msakac@foi.hr>
2022-01-03 11:12:35 +0100

    Added Consumer<Cart> on a Badge. When we add a new item to our cart this widget rebuilds.

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
-
-
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorites,
@@ -17,7 +18,6 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-
   bool _showOnlyFavorites = false;
   @override
   Widget build(BuildContext context) {
@@ -29,20 +29,33 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             onSelected: (FilterOptions selectedValue) {
               setState(() {
                 print(selectedValue);
-              if(selectedValue == FilterOptions.Favorites){
-                _showOnlyFavorites = true;
-              }else{
-                _showOnlyFavorites = false;
-              }
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
               });
             },
             icon: Icon(
               Icons.more_vert,
             ),
             itemBuilder: (_) => [
-              PopupMenuItem(child: Text('Only favorites'), value: FilterOptions.Favorites),
+              PopupMenuItem(
+                  child: Text('Only favorites'),
+                  value: FilterOptions.Favorites),
               PopupMenuItem(child: Text('Show all'), value: FilterOptions.All),
             ],
+          ),
+          //builder: (ctx, product, child)
+          Consumer<Cart>( //IconButton je child od Consumer jer ga netrebamo rebuildati i damo ga badgu kao child ch
+            builder: (_, cartData, ch) => Badge(
+              child: ch,
+              value: cartData.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: (){},
+            ),
           ),
         ],
       ),
