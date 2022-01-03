9b7bd398c493aee6f6d36ef03bd881d81388bc11
msakac <msakac@foi.hr>
2022-01-03 14:13:25 +0100

    Added new route to OrdersScreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import './providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
 import './providers/orders.dart';
+import './screens/orders_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -44,6 +46,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
