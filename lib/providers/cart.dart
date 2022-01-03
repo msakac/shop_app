87a7b6d2c15284b02e6b06d46d3baa04e479f8ed
msakac <msakac@foi.hr>
2022-01-03 11:11:57 +0100

    Added itemCount getter that gets the amount of products we have in our cart.

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -16,10 +16,15 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
+  }
+
+  int get itemCount{
+
+    return _items.length;
   }
 
   void addItem(String productId, double price, String title) {
@@ -44,5 +49,6 @@ class Cart with ChangeNotifier {
             quantity: 1),
       );
     }
+    notifyListeners();
   }
 }
