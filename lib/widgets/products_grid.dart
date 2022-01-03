9fe7ceb4d1c66980f4d316ddc0f82846182db8a0
msakac <msakac@foi.hr>
2022-01-03 10:04:38 +0100

    Created new property showFavorites and a constructor that receives this boolean. Depending on boolean we call productsData.favoriteItems or productsData.items from products provider class

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,12 +5,16 @@ import '../providers/products.dart';
 import '../widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+
+  final bool showFavorites;
+  ProductsGrid(this.showFavorites);
+
   @override
   Widget build(BuildContext context) {
     final productsData =
         Provider.of<Products>(context); //direct comm chanel to products class
     //vraca se u products overview_Screen, pa u main i tam pronalazi ChangeNotifierProvider tipa Producst()
-    final products = productsData.items;
+    final products = showFavorites ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
