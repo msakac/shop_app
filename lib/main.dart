a81c0a9742311c7629cefb623cdbd1514b809628
msakac <msakac@foi.hr>
2022-01-03 07:22:49 +0100

    main.dart now returns ChangeNotifierProvider. This widget takes builder with context and a Provider class (Products). This notifier is at the root because we will use it both in ProductsOverviewScreen and in ProductDetailScreen. This way this listener is available in both screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, missing_required_param
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 
 void main() => runApp(MyApp());
@@ -10,17 +13,21 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider( //provider klasa
+      builder: (ctx) => Products(), //provider verzija 4.x.x, create umjesto builder
+      //mjenjali se budu samo oni widget koji slusaju providera
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
