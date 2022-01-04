eac73c5ef2d5ab562e576739ed3f5f4de4e29e9a
msakac <msakac@foi.hr>
2022-01-04 09:45:08 +0100

    Added AppDrawer() widget to the Scaffold drawer

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -19,6 +20,7 @@ class UserProductsScreen extends StatelessWidget {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
