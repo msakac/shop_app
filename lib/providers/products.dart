ff1fadea982057902c8a9207f49d9fd326222716
msakac <msakac@foi.hr>
2022-01-10 15:05:50 +0100

    UpdateProduct now updates in database

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -111,12 +111,21 @@ class Products with ChangeNotifier {
     //ovdje se dodaju novi produkti. Promjene se dogadaju samo unutar klase tak da moremo koristiti notify listeners
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((element) => element.id == id);
     if (prodIndex >= 0) {
+          final url = Uri.parse(
+        'https://shop-app-6ca39-default-rtdb.firebaseio.com/products/$id.json');
+        await http.patch(url, body: json.encode({
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'price': newProduct.price,
+          'imageUrl': newProduct.imageUrl
+        }));
       _items[prodIndex] = newProduct;
+      notifyListeners();
     } else {}
-    notifyListeners();
+    
   }
 
   void removeProduct(String id) {
